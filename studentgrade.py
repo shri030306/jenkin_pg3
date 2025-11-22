8ae3be9bcc806ce7ac485374bf4ed3af20d08091
shri030306 <shrilakshmigs03@gmail.com>
2025-11-22 12:09:54 +0530

    Update studentgrade.py

--- studentgrade.py
+++ studentgrade.py
@@ -1,8 +1,14 @@
-marks1 = float(input("Enter marks of subject 1: "))
-marks2 = float(input("Enter marks of subject 2: "))
-marks3 = float(input("Enter marks of subject 3: "))
-marks4 = float(input("Enter marks of subject 4: "))
-marks5 = float(input("Enter marks of subject 5: "))
+import sys
+
+if len(sys.argv) != 6:
+    print("Usage: python studentgrade.py mark1 mark2 mark3 mark4 mark5")
+    sys.exit(1)
+
+marks1 = float(sys.argv[1])
+marks2 = float(sys.argv[2])
+marks3 = float(sys.argv[3])
+marks4 = float(sys.argv[4])
+marks5 = float(sys.argv[5])
 
 avg = (marks1 + marks2 + marks3 + marks4 + marks5) / 5
 
@@ -14,11 +20,12 @@ elif avg >= 55:
     grade = "C"
 elif avg >= 40:
     grade = "D"
-elif avg >= 30:   
+elif avg >= 30:
     grade = "E"
 else:
     grade = "Fail"
 
 print("\n--- Result ---")
+print("Marks:", marks1, marks2, marks3, marks4, marks5)
 print("Average Marks:", avg)
 print("Grade:", grade)
