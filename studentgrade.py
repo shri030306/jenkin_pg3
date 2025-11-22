3e79d178030c7098deb97dd4bffa6cbe6c8851e9
shri030306 <shrilakshmigs03@gmail.com>
2025-11-22 11:46:15 +0530

    Create studentgrade.py

---
+++ studentgrade.py
@@ -0,0 +1,19 @@
+marks1 = float(input("Enter marks of subject 1: "))
+marks2 = float(input("Enter marks of subject 2: "))
+marks3 = float(input("Enter marks of subject 3: "))
+
+avg = (marks1 + marks2 + marks3) / 3
+
+if avg >= 85:
+    grade = "A"
+elif avg >= 70:
+    grade = "B"
+elif avg >= 55:
+    grade = "C"
+elif avg >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Average Marks:", avg)
+print("Grade:", grade)
